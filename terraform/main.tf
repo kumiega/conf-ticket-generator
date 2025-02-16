5c607adcf0c571c778458c731ec40e823f9e021d
Tomasz Kumięga <poczta@kumiega.org>
2025-02-16 23:36:24 +0100

    feat: adjust terraform aws infra

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,3 +20,13 @@ terraform {
 provider "aws" {
   region = "eu-central-1"
 }
+
+module "perf" {
+  source = "./perf"
+
+  lambda_function_name = "website_perf_check"
+
+  # Important - adjust lambda path to your project structure
+  lambda_source_dir = "${path.root}/../lib/lambda-perf"
+
+}
